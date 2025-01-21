35c83d0ac068f4e0a1a64c951ae57608b222471d
ycui <ycui@specialtycircuits.com>
2025-01-20 22:49:08 -0800

    fix volume issue on iphone, set full volume adjustment range to 32dB

--- src/core/xua_defs.h
+++ src/core/xua_defs.h
@@ -112,8 +112,15 @@
 #define OUTPUT_VOLUME_CONTROL       (1)
 #define INPUT_VOLUME_CONTROL        (1)
 
-#define VOLUME_RES                  (0x40) // The resolution of the volume control in db as a 8.8 fixed point number
-                                            // Default: 0x100 (1db)
+#define VOLUME_RES                  (0x0020)  //  Default: 0x40 (0.5db). The resolution of the volume control in db as a 8.8 fixed point number
+                                            
+#ifndef MIN_VOLUME
+#define MIN_VOLUME                  (0xE000)    //  Default: 0xE100 (-32db). The minimum volume setting. This is a signed 8.8 fixed point number.
+#endif
+
+#ifndef MAX_VOLUME
+#define MAX_VOLUME                  (0x0000)    //  Default: 0x0000 (0db). The maximum volume setting. This is a signed 8.8 fixed point number.
+#endif
 
 #ifndef DFU_PID
     #if (AUDIO_CLASS == 1)
